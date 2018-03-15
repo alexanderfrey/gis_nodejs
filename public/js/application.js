e4efc56fc6a44e4dcd19215d786c04d55f8634eb
Alexander Frey <alexander@buschnerfrey.com>
2018-03-15 18:13:20 +0100

    Added D3 projection

--- public/js/application.js
+++ public/js/application.js
@@ -3,23 +3,12 @@ $(function() {
 	var userId = Math.random().toString(16).substring(2,15);
 	var socket = io.connect("/");
 	var map;
+	var center = [52.52604901080316, 13.403679728507996];
+	var projection;
+	var mouseCircle;
 
 	var info = $("#infobox");
 	var doc = $(document);
-
-	// custom marker's icon styles
-	var tinyIcon = L.Icon.extend({
-		options: {
-			shadowUrl: "../assets/marker-shadow.png",
-			iconSize: [25, 39],
-			iconAnchor:   [12, 36],
-			shadowSize: [41, 41],
-			shadowAnchor: [12, 38],
-			popupAnchor: [0, -30]
-		}
-	});
-	var redIcon = new tinyIcon({ iconUrl: "../assets/marker-red.png" });
-	var yellowIcon = new tinyIcon({ iconUrl: "../assets/marker-yellow.png" });
 
 	var sentData = {boxes:[]};
 
@@ -30,8 +19,9 @@ $(function() {
 
 	socket.on("load:boxes", function(data) {
 		// remember users id to show marker only once
-    //console.log(data.id);
+    console.log('Receiving new data: ' + data.boxes.length);
 		sentData = data;
+		updateMarker(sentData);
 
 		// connects[data.id] = data;
 		// connects[data.id].updated = $.now(); // shorthand for (new Date).getTime()
@@ -40,39 +30,149 @@ $(function() {
 	function initMap() {
 		// load leaflet map
 		map = L.map("map", {
-        center: [51.505, -0.09],
-        zoom: 19
+        center: center,
+        zoom: 18
     });
 
 		// leaflet API key tiler
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        maxZoom: 22,
-        id: 'mapbox.streets',
-        accessToken: 'pk.eyJ1IjoibWFidXMiLCJhIjoiY2lwd2xnMHZ5MDA1c2k3bm9wc2J2dnRjZyJ9.lHIMqnnD4gx9z6ovNgzcmg'
-    }).addTo(map);
+		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+				attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+				maxZoom: 18,
+				id: 'mapbox.streets',
+				accessToken: 'pk.eyJ1IjoibWFidXMiLCJhIjoiY2lwd2xnMHZ5MDA1c2k3bm9wc2J2dnRjZyJ9.lHIMqnnD4gx9z6ovNgzcmg'
+		}).addTo(map);
+
+		markerGroup = L.layerGroup().addTo(map);
+
+		mouseCircle = L.circle([52.52604901080316, 13.403679728507996], {
+				color: 'blue',
+				fillColor: '#f03',
+				fillOpacity: 0.5,
+				radius: 1
+		}).addTo(markerGroup);
+	}
+
+	function initD3() {
+		var svg = d3.select(map.getPanes().overlayPane).append("svg"),
+    g = svg.append("g").attr("class", "leaflet-zoom-hide");
+		var w = 600;
+		//Define map projection
+		projection = d3.geoMercator()
+								 .center([ 13.403679728507996, 52.52604901080316 ])
+								 .translate([ 0,0 ])
+								 .scale([ w/0.163 ]);
+
+		 //Load in GeoJSON data
+ 		d3.json("../assets/map.geojson", function(error, collection) {
+
+ 			var transform = d3.geoTransform({point: projectPoint});
+ 			var path = d3.geoPath()
+ 							 .projection(transform);
+
+ 			var feature = g.selectAll("path")
+ 				.data(collection.features)
+ 				.enter().append("path");
+ 			map.on("zoom", reset);
+ 			reset();
+
+
+ 			function reset() {
+ 				var bounds = path.bounds(collection),
+ 					topLeft = bounds[0],
+ 					bottomRight = bounds[1];
+
+ 				var width = bottomRight[0] - topLeft[0];
+ 				var height = bottomRight[1] - topLeft[1];
+
+ 				projection.fitExtent([[0, 0], [width, height]], collection);
+
+				svg.attr("width", width)
+				.attr("height", height)
+				.style("left", topLeft[0] + "px")
+				.style("top", topLeft[1] + "px");
+
+				g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+
+				feature.attr("d", path);
+ 			}
+
+		});
+
+		// Attach a listener for the mousemove event
+		svg.on('click', function(ev) {
+
+			// Get the (x, y) position of the mouse (relative to the SVG element)
+			var pos = d3.mouse(svg.node()),
+					px = pos[0],
+					py = pos[1];
+
+			var data = {id:Math.random().toString(16).substring(2,15),boxes: [pos]};
+
+			socket.emit('send:boxes', data);
+
+			// Compute the corresponding geographic coordinates using the inverse projection
+			// var coords = projection.invert([px, py]);
+			//
+			// // Format the coordinates to have at most 4 decimal places
+			// var lng = coords[0].toFixed(8),
+			// 		lat = coords[1].toFixed(8);
+			//
+			// var newLatLng = new L.LatLng(lat, lng);
+			// mouseCircle.setLatLng(newLatLng);
+
+			// Set the content of the label
+		//	console.log([lat, lng].join(', '));
+		 // label.text([lat, lon].join(', '));
+		});
+	}
+
+	function projectPoint(x, y) {
+		var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+		this.stream.point(point.x, point.y);
 	}
 
 
 	function updateMarker(data) {
-    if(markerGroup){
-      map.removeLayer(markerGroup);
-    }
+    markerGroup.clearLayers();
 
-    markerGroup = L.layerGroup().addTo(map);
+
 		for (i = 0; i < data.boxes.length; i++) {
       let box = data.boxes[i];
+			var px = box[0];
+			var py = box[1];
+
+
+			var coords = projection.invert([px, py]);
+
+			var lng = coords[0].toFixed(8),
+			 		lat = coords[1].toFixed(8);
+
+			var newLatLng = new L.LatLng(lat, lng);
+			var marker = createMarkerAt(newLatLng);
+
+			marker.addTo(map);
 
 
       console.log(box);
-			var marker = L.marker([box[1], box[0]], { icon: yellowIcon }).addTo(markerGroup);
+
 			//markers[data.id] = marker;
 		}
 	}
 
+	function createMarkerAt(latLng){
+		return circle = L.circle(latLng, {
+				color: 'blue',
+				fillColor: '#f03',
+				fillOpacity: 0.5,
+				radius: 1
+		});
+	}
+
   initMap();
+	initD3();
 
 	// delete inactive users every 15 sec
 	setInterval(function() {
-		updateMarker(sentData);
+		//updateMarker(sentData);
   }, 1000);
 });
